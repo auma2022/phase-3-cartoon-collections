c69352f316e09299765f0afca31c244cd652344c
auma2022 <dianaauma2@gmail.com>
2022-08-26 12:38:56 +0300

    commit tree cf99ab9a6205291adb424eba9c958410323e5482

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (dwarves)
   # Your code here
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (array)
   # Your code here
+  result = []
+  array.each do |elem|
+    result << elem.capitalize+ '!' 
+  end
+
+  result
+
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  if array.any?{|call| call.size > 4 }
+   return true 
+  elsif array.any?{|call| call.size <= 4}
+   return false
+  end
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(potential_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+   potential_cheese.find do |elem| 
+    cheese_types.include?(elem)
+
+   end
+   
 end
+ puts find_the_cheese (["chedda", "gouda", "camembert"])
